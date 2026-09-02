3231ac804fd2db79f92ac6624a06f6848ffdcdff
Mykhailo <mykhailoshpeh@gmail.com>
2026-09-02 16:50:23 +0300

    basic settings changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 //todo: https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/airplane-model-store3-state2-form1-lifeCycle/', //! <-- ім'я репозиторію
+  base: '/airplane-model-store3-state2-form1-lifeCycle-HTTPrequests/', //! <-- ім'я репозиторію
   //! Налаштування Аліасів для абсолютних шляхів імпортів
   resolve: {
     alias: {
