38489681a6cd53d7e3adbb60025429526248384c
Mykhailo <mykhailoshpeh@gmail.com>
2026-09-09 17:12:02 +0300

    rendered pokemon img and name

--- src/components/Pokemon/PokemonInfo.jsx
+++ src/components/Pokemon/PokemonInfo.jsx
@@ -5,7 +5,7 @@ import css from './Pokemoninfo.module.css';
 export class PokemonInfo extends Component {
     state = {
         pokemon: null, //! об'єкт з даними про Покемона
-
+        loading: false //! індикатор завантаження (лоадер)
     }
 
 
@@ -14,10 +14,13 @@ export class PokemonInfo extends Component {
         const prevName = prevProps.pokemonName;
         const nextName = this.props.pokemonName; //! ім'я покемона (оновлене)
 
+
         if (prevName != nextName) {
             console.log("❗️Змінилося ім'я ПОКЕМОНА");
             console.log("⏮️prevName (prevProps.pokemonName): ", prevProps.pokemonName);
             console.log("⏭️nextName (this.props.pokemonName): ", this.props.pokemonName);
+
+            this.setState({ loading: true }); //! індикатор завантаження (лоадер) 
 
             //todo робимо запит 
             // fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
@@ -26,7 +29,7 @@ export class PokemonInfo extends Component {
                     .then(res => res.json())
                     // .then(pokemon => console.log("pokemon:", pokemon))
                     .then(pokemon => this.setState({ pokemon }))
-                // .finally(() => this.setState({ loading: false }));
+                    .finally(() => this.setState({ loading: false })); //! індикатор завантаження (лоадер) змніюємо на false
 
             }, 2000);
         }
@@ -35,25 +38,31 @@ export class PokemonInfo extends Component {
     render() {
 
         const {
-            pokemon
+            pokemon,
+            loading
         } = this.state
 
         console.log("----------------------------------------------");
         console.log("ℹ️🐷 Покемон:", pokemon);
-        // console.log("ℹ️⏳ Індикатор завантаження (лоадер):", loading);
+        console.log("ℹ️⏳ Індикатор завантаження (лоадер):", loading);
         console.log("----------------------------------------------");
 
 
         return (
             <div className={css.pokemonInfo}>
                 <h1>PokemonInfo</h1>
-                <h2><i>Введіть ім'я покемона</i></h2>
+                {!pokemon && <h2><i>Введіть ім'я покемона</i></h2>}
                 <h2><u><i>Ви ввели ім'я покемона</i></u>: <b>???</b></h2>
-                <h2 className={css.pokemonInfoLoading}>Завантажуємо покемон...</h2>
-                <div className={css.pokemonContainer}>
-                    <p>Покемон: <span className={css.pokemonName}></span></p>
-                    <img src="./" alt="pokemon" />
-                </div>
+                {loading && <h2 className={css.pokemonInfoLoading}>Завантажуємо покемон...</h2>}
+                
+                {pokemon && <div className={css.pokemonContainer}>
+                   <p className={css.pokemonName}><u><i>Покемон</i></u>: <b>{pokemon.name}</b></p>
+                    <img
+                        src={pokemon.sprites.other.home.front_default}
+                        width="300"
+                        alt={pokemon.name}
+                    />
+                </div>}
             </div>
         )
     }
