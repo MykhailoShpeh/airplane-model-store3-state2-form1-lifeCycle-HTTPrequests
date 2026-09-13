0463504cc97120726ff7fbf5dedee0cb958c2eea
Mykhailo <mykhailoshpeh@gmail.com>
2026-09-13 11:38:42 +0300

    state machine

--- src/components/Pokemon/PokemonInfoAndErrorsStateMachine1.jsx
+++ src/components/Pokemon/PokemonInfoAndErrorsStateMachine1.jsx
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 
 import css from './Pokemoninfo.module.css';
 
+//? Застосуємо такі статуси:
+//?     - idle - запиту ще немає, нічого не відбувається
+//?     - pending - пішов запит
+//?     - rejected - відповідь на запит з помилкою
+//?     - resolved - успішна відповідь на запит
+
+
 export class PokemonInfoAndErrorsStateMachine1 extends Component {
     state = {
         pokemon: null, //! об'єкт з даними про Покемона
-        loading: false, //! індикатор завантаження (лоадер)
-        error: null //todo: Обробка помилок
-
+        // loading: false, //! індикатор завантаження (лоадер)
+        error: null, //todo: Обробка помилок
+        status: 'idle', //! статус
     }
 
 
@@ -22,11 +29,14 @@ export class PokemonInfoAndErrorsStateMachine1 extends Component {
             console.log("⏮️prevName (prevProps.pokemonName): ", prevProps.pokemonName);
             console.log("⏭️nextName (this.props.pokemonName): ", this.props.pokemonName);
 
-            this.setState({
-                pokemon: null, //! прибираємо попереднього покемона при завантаженні наступного
-                loading: true, //! індикатор завантаження (лоадер)
-                error: null, //todo: Обробка помилок - прибираємо можливу попередню помилку
-            });
+            //todo: old
+            // this.setState({
+            //   pokemon: null, //! прибираємо попереднього покемона при завантаженні наступного
+            //   // loading: true, //! індикатор завантаження (лоадер)
+            //   error: null, //todo: Обробка помилок - прибираємо можливу попередню помилку
+            // });
+            //*: New
+            this.setState({ status: 'pending' }); //! статус
 
 
 
@@ -46,16 +56,21 @@ export class PokemonInfoAndErrorsStateMachine1 extends Component {
                     .then(pokemon =>
                         this.setState({
                             pokemon,
-                            error: null, // todo: Обробка помилок
+                            // error: null, // todo: Обробка помилок - вже не потрібно
+                            status: 'resolved', //! статус 
                         }))
 
                     .catch(error => {
                         this.setState({
+                            // pokemon: null, // todo: вже не потрібно
                             error,
-                            pokemon: null
-                        })})
+                            status: 'rejected', //! статус 
+                        })
+                    })
 
-                    .finally(() => this.setState({ loading: false })); //! індикатор завантаження (лоадер) змніюємо на false
+                    .finally(() => this.setState({
+                        // loading: false
+                    })); //! індикатор завантаження (лоадер) змніюємо на false
 
             }, 3000);
         }
@@ -65,8 +80,9 @@ export class PokemonInfoAndErrorsStateMachine1 extends Component {
 
         const {
             pokemon,
-            loading,
-            error
+            // loading,
+            error,
+            status
         } = this.state
 
         const {
@@ -76,41 +92,95 @@ export class PokemonInfoAndErrorsStateMachine1 extends Component {
         console.log("----------------------------------------------");
         console.log("ℹ️props-> 🐷 Покемон-ім'я:", pokemonName);
         console.log("ℹ️{🐷} Покемон-об'єкт:", pokemon);
-        console.log("ℹ️⏳ Індикатор завантаження (лоадер):", loading);
+        // console.log("ℹ️⏳ Індикатор завантаження (лоадер):", loading);
         console.log("ℹ️❌ Помилка:", error);
         console.log("----------------------------------------------");
 
 
-        return (
-            <div className={css.pokemonInfo}>
-                <h1>PokemonInfo</h1>
+        // return (
+        //     <div className={css.pokemonInfo}>
+        //         <h1>PokemonInfo</h1>
 
-                {/* //todo: Обробка помилок */}
-                {/* {error && <h2>Покемена з ім'ям {pokemonName} не існує</h2>} */}
-                {error &&
-                    <h2 className={css.pokemonInfoTitleError}>{error.message}</h2>}
+        //         {/* //todo: Обробка помилок */}
+        //         {/* {error && <h2>Покемена з ім'ям {pokemonName} не існує</h2>} */}
+        //         {error &&
+        //             <h2 className={css.pokemonInfoTitleError}>{error.message}</h2>}
 
-                {!error && !loading && !pokemon &&
-                    <h2><i>Введіть ім'я покемона</i></h2>}
+        //         {!error && !loading && !pokemon &&
+        //             <h2><i>Введіть ім'я покемона</i></h2>}
 
-                {pokemon &&
-                    <h2><u><i>Ви ввели ім'я покемона</i></u>: <b>{pokemonName}</b></h2>}
+        //         {pokemon &&
+        //             <h2><u><i>Ви ввели ім'я покемона</i></u>: <b>{pokemonName}</b></h2>}
 
-                {loading &&
-                    <h2 className={css.pokemonInfoLoading}>Завантажуємо покемон...</h2>}
+        //         {loading &&
+        //             <h2 className={css.pokemonInfoLoading}>Завантажуємо покемон...</h2>}
 
-                {pokemon &&
+        //         {pokemon &&
+        //             <div className={css.pokemonContainer}>
+        //                 <p className={css.pokemonName}><u><i>Покемон</i></u>: <b>{pokemon.name}</b></p>
+        //                 <img
+        //                     // src={pokemon.sprites.other.home.front_default} //todo: var.1
+        //                     src={pokemon.sprites.other["official-artwork"].front_default}  //todo: var.2
+        //                     width="300"
+        //                     alt={pokemon.name}
+        //                 />
+        //             </div>}
+        //     </div>
+        // )
+
+        //*: New
+        //? Застосуємо такі статуси:
+        //?     - idle - запиту ще немає, нічого не відбувається
+        //?     - pending - пішов запит
+        //?     - rejected - відповідь на запит з помилкою
+        //?     - resolved - успішна відповідь на запит
+
+        //? idle - запиту ще немає, нічого не відбувається
+        if (status === 'idle') {
+            return (
+                <div className={css.pokemonInfo}>
+                    <h1>PokemonInfo</h1>
+                    <h2><i>Введіть ім'я покемона</i></h2>
+                </div>
+            )
+        }
+
+        //? pending - пішов запит
+        if (status === 'pending') {
+            return (
+                <div className={css.pokemonInfo}>
+                    <h1>PokemonInfo</h1>
+                    <h2><u><i>Ви ввели ім'я покемона</i></u>: <b>{pokemonName}</b></h2>
+                    <h2 className={css.pokemonInfoLoading}>Завантажуємо покемон...</h2>
+                </div>
+            );
+        };
+
+        //? rejected - прийшла відповідь на запит з помилкою
+        if (status === 'rejected') {
+            return (
+                <div className={css.pokemonInfo}>
+                    <h1>PokemonInfo</h1>
+                    <h2 className={css.pokemonInfoTitleError}>{error.message}</h2>
+                </div>
+            )
+        }
+
+        //? resolved - успішна відповідь на запит
+        if (status === 'resolved') {
+            return (
+                <div className={css.pokemonInfo}>
+                    <h1>PokemonInfo</h1>
                     <div className={css.pokemonContainer}>
                         <p className={css.pokemonName}><u><i>Покемон</i></u>: <b>{pokemon.name}</b></p>
                         <img
-                            // src={pokemon.sprites.other.home.front_default} //todo: var.1
                             src={pokemon.sprites.other["official-artwork"].front_default}  //todo: var.2
                             width="300"
                             alt={pokemon.name}
                         />
-                    </div>}
-            </div>
-        )
+                    </div>
+                </div>
+            )
+        }
     }
-
 }
