c2306ffdbbc9b6ba2cdd9f4b1bcf070c6bfe170d
Mykhailo <mykhailoshpeh@gmail.com>
2026-09-16 17:23:30 +0300

    new app for state machine

--- src/components/App/AppHTTPRequestAdvanced.jsx
+++ src/components/App/AppHTTPRequestAdvanced.jsx
@@ -7,62 +7,70 @@
 // https://pokeapi.co/api/v2/pokemon
 
 import React, { Component } from "react";
-// https://www.npmjs.com/package/react-toasti
-// https://fkhadra.github.io/react-toastify/introduction/
-import { ToastContainer } from 'react-toastify'; //! 01.Підлючення бібліотеки react-toastify 
+
 import { PokemonForm } from '@/components/Pokemon/PokemonForm.jsx'; //! форма для отримання ім'я покемона
 import { PokemonInfo } from '@/components/Pokemon/PokemonInfo.jsx'; //! компонент, який приймає ім'я покемона, робить запит та будує розмітку 
 // import { PokemonInfoAndErrors } from '@/components/Pokemon/PokemonInfoAndErrors.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки 
-import { PokemonInfoAndErrorsStateMachine1 } from '@/components/Pokemon/PokemonInfoAndErrorsStateMachine1.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки + State Machine1
-// import { PokemonInfoAndErrorsStateMachine21 } from '@/components/Pokemon/PokemonInfoAndErrorsStateMachine21.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки + State Machine2-1
-// import { PokemonInfoAndErrorsStateMachine22 } from '@/components/Pokemon/PokemonInfoAndErrorsStateMachine22.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки + State Machine2-1
+import { PokemonInfoAndErrorsStateMachine1 } from '@/components/Pokemon/PokemonInfoAndErrorsStateMachine1.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки + State Machine
+// import { PokemonInfoAndErrorsStateMachine21 } from '@/components/Pokemon/PokemonInfoAndErrorsStateMachine21.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки + State Machine (чистий код)
+// import { PokemonInfoAndErrorsStateMachine22 } from '@/components/Pokemon/PokemonInfoAndErrorsStateMachine22.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки + State Machine + Рефакторинг + React-skeleton
+// import { PokemonInfoAndErrorsStateMachine23 } from '@/components/Pokemon/PokemonInfoAndErrorsStateMachine23.jsx'; //! /! компонент, який приймає ім'я покемона, робить запит, будує розмітку та обробляє помилки + State Machine + Рефакторинг + React-skeleton (чистий код) 
 
-import css from './AppHTTPRequestAdvanced.module.css'
+import { ToastContainer } from 'react-toastify'; //! 01.Підлючення бібліотеки react-toastify
+// https://www.npmjs.com/package/react-toasti
+// https://fkhadra.github.io/react-toastify/introduction/
+
+import css from "./AppHTTPRequestAdvanced.module.css";
+
 
 export class AppHTTPRequestAdvanced extends Component {
-    state = {
-        pokemonName: null
-    }
+  state = {
+    pokemonName: "", //! ім'я покемона
+  };
 
-    submitForm = (pokemonName) => {
-        console.log("✅Дані з форми PokemonForm:", pokemonName);
-        this.setState({
-            pokemonName
-        })
-    }
+  submitForm = (pokemonName) => {
+    // console.log("✅Дані з форми PokemonForm:", pokemonName);
+    this.setState({
+      pokemonName
+    });
+  };
 
-    render() {
 
-        const { pokemonName } = this.state
+  render() {
+      const {
+        pokemonName,
+      } = this.state;
+  
+      console.log("----------------------------------------------");
+      console.log("✅🐷 Ім'я покемона:", pokemonName);
+      console.log("----------------------------------------------");
+    
+      return (
+        <div className={css.mainContainer} >
+          {/* //! Форма для отримання ім'я покемона */}
+          <PokemonForm onSubmit={this.submitForm} />
 
-        console.log("----------------------------------------------");
-        console.log("✅🐷 Ім'я покемона:", pokemonName);
-        console.log("----------------------------------------------");
+          {/* //! HTTP-запит + Розмітка */}
+          {/* <PokemonInfo pokemonName={pokemonName} /> */}
 
-        return (
-            <div className={css.mainContainer} >
-                {/* //! Форма для отримання ім'я покемона */}
-                <PokemonForm onSubmit={this.submitForm} />
+          {/* //! HTTP-запит + Розмітка + Обробка помилок */}
+          {/* <PokemonInfoAndErrors pokemonName={pokemonName} /> */}
 
-                {/* //! HTTP-запит + Розмітка */}
-                {/* <PokemonInfo pokemonName={pokemonName} /> */}
+          {/* //! HTTP-запит + Розмітка + Обробка помилок + State Machine */}
+          <PokemonInfoAndErrorsStateMachine1 pokemonName={pokemonName} />
 
-                {/* //! HTTP-запит + Розмітка + Обробка помилок */}
-                {/* <PokemonInfoAndErrors pokemonName={pokemonName} /> */}
+          {/* //! HTTP-запит + Розмітка + Обробка помилок + State Machine (чистий код) */}
+          {/* <PokemonInfoAndErrorsStateMachine21 pokemonName={pokemonName} /> */}
 
-                {/* //! HTTP-запит + Розмітка + Обробка помилок + State Machine1 */}
-                <PokemonInfoAndErrorsStateMachine1 pokemonName={pokemonName} />
+          {/* //! HTTP-запит + Розмітка + Обробка помилок + State Machine + Рефакторинг + React-skeleton */}
+          {/* <PokemonInfoAndErrorsStateMachine22 pokemonName={pokemonName} /> */}
 
-                {/* //! HTTP-запит + Розмітка + Обробка помилок + State Machine2-1 */}
-                {/* <PokemonInfoAndErrorsStateMachine21 pokemonName={pokemonName} /> */}
-
-                {/* //! HTTP-запит + Розмітка + Обробка помилок + State Machine2-2 */}
-                {/* <PokemonInfoAndErrorsStateMachine22 pokemonName={pokemonName} /> */}
-
-                {/* //! 01.Підлючення бібліотеки react-toastify */}
-                <ToastContainer autoClose={2000} />
-            </div>
-
-        )
-    }
-}
+          {/* //! HTTP-запит + Розмітка + Обробка помилок + State Machine + Рефакторинг + React-skeleton (чистий код) */}
+          {/* <PokemonInfoAndErrorsStateMachine23 pokemonName={pokemonName} /> */}
+          
+          {/* //! 01.Підлючення бібліотеки react-toastify */}
+          <ToastContainer autoClose={2000} /> 
+        </div>
+      );
+    };
+};
