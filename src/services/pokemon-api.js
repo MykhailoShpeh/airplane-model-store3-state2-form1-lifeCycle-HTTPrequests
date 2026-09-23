968389df65ad1ce52975f331a2e2d51bb87ddea6
Mykhailo <mykhailoshpeh@gmail.com>
2026-09-23 17:49:05 +0300

    axios

--- src/services/pokemon-api.js
+++ src/services/pokemon-api.js
@@ -1,19 +1,70 @@
-function fetchPokemon(name) {
+import axios from "axios";
 
 
+//todo: old (fetch)
+// function fetchPokemon(name) {
+//     return (
+//         fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+//             .then(response => {
+//                 if (response.ok) {
+//                     return response.json()
+//                 };
+//                 return Promise.reject(new Error(`Покемена з ім'ям «${name}» не існує`))
+//             }))
+// }
 
-    return (
-        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-            .then(response => {
-                if (response.ok) {
-                    return response.json()
-                };
-                return Promise.reject(new Error(`Покемена з ім'ям «${name}» не існує`))
-            }))
-}
+//? Axios сам вважає помилкою HTTP-відповіді з кодами поза 2xx.
+//? Тому ручна перевірка response.ok та Promise.reject(...) більше не потрібні.
+//? Axios автоматично поміщає JSON-відповідь у response.data.
+
+//* NEW-1 (axios)
+// function fetchPokemon(name) {
+//   return axios
+//     .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+//     .then(response => response.data);
+// };
+
+//* NEW-2 (axios + обробка помилок (формуємо власну помилку))
+// function fetchPokemon(name) {
+//   return axios
+//     .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+//     .then(response => response.data)
+//     .catch(error => {
+//       //! Помилка: 404
+//       if (error.response?.status === 404) {
+//         throw new Error(`Покемена з ім'ям «${name}» не існує`);
+//       };
+//       //! Інша помилка
+//       throw error;
+//     });
+// };
+
+//* NEW-3 (axios + обробка помилок (формуємо власні помилки) + async/await)
+async function fetchPokemon(name) {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    console.log("🅰️xios==>✅response:", response);
+    return response.data;
+  } catch (error) {
+    //! Помилка: 404
+    if (error.response?.status === 404) {
+      console.log("🅰️xios==>❌error-404", error);
+      throw new Error(`Покемена з ім'ям «${name}» не існує`);
+    };
+    //! Помилка: 400
+    if (error.response?.status === 400) {
+      console.log("🅰️xios==>❌error-400:", error);
+      throw new Error(`Сталася синтаксична помилка при введенні імені «${name}»`);
+    };
+    //! Інша помилка
+    console.log("🅰️xios==>❌error-(інша помилка):", error);
+    throw error;
+  };
+};
+
 
 const api = {
-  fetchPokemon,
+    fetchPokemon,
 };
 
 export default api;
